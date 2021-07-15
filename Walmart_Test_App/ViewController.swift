9cd74f76a62db21ecfebff9bbb3408989c1123df
Igor Chernobai <tiv.iosdeveloper@gmail.com>
2021-07-15 08:02:07 -0700

    First commit, project created

---
+++ Walmart_Test_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Walmart_Test_App
+//
+//  Created by Igor Chernobai on 7/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
